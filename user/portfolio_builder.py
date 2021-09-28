de75dc9c426a941bc97e1d79f451937376b14e18
rdillens <remydillenseger@gmail.com>
2021-09-28 12:43:06 -0500

    fixed bug w stock, updated portfolio dictionary

--- user/portfolio_builder.py
+++ user/portfolio_builder.py
@@ -2,33 +2,47 @@ import questionary
 import finnhubIO as fh
 import pandas as pd
 
+
 market_list = ['stock', 'crypto']
 
-def build_portfolio(dict):
-    market = choose_market()
-    if market == 'crypto':
-        exchange = choose_crypto_exchange()
-        ticker_list = fh.get_crypto_tickers(exchange=exchange)
-        ticker_df = pd.DataFrame(ticker_list)
-        ticker_df['baseCurrency'] = ticker_df['displaySymbol'].apply(lambda x: x[x.find('/')+1:])
-        ticker_df['quoteCurrency'] = ticker_df['displaySymbol'].apply(lambda x: x[:x.find('/')])
 
-        while(questionary.confirm("Add a product?").ask()):
+def build_portfolio(dict):
+    while(questionary.confirm("Add a product?").ask()):
+        product_dict = {}
+        market = choose_market()
+        product_dict['market'] = market
+        # Add crypto market item to portfolio
+        if market == 'crypto':
+            exchange = choose_crypto_exchange()
+            product_dict['exchange'] = exchange
+            ticker_list = fh.get_crypto_tickers(exchange=exchange)
+            ticker_df = pd.DataFrame(ticker_list)
+            ticker_df['baseCurrency'] = ticker_df['displaySymbol'].apply(lambda x: x[x.find('/')+1:])
+            ticker_df['quoteCurrency'] = ticker_df['displaySymbol'].apply(lambda x: x[:x.find('/')])
+
             ticker = choose_crypto(ticker_df)
-            dict['portfolio'].append(ticker)
-    if market == 'stock':
-        stock_type = questionary.select(
-            "What type of stock?",
-            choices=fh.stocks_df['type'].unique()
-        )
-        symbol = questionary.select(
-            "What symbol?",
-            choices=fh.stocks_df['symbol'].unique(),
-        )
-        print(symbol)
-        # dict['portfolio'].append(symbol)
+            product_dict['ticker'] = ticker
+            print(product_dict)
+            dict['portfolio'].append(product_dict)
+
+        # Add stock market item to portfolio
+        if market == 'stock':
+            product_dict['exchange'] = 'US'
+            stock_type = questionary.select(
+                "What type of stock?",
+                choices=fh.stocks_df['type'].unique()
+            ).ask()
+
+            ticker = questionary.select(
+                "What symbol?",
+                choices=fh.stocks_df['symbol'].unique(),
+            ).ask()
+            product_dict['ticker'] = ticker
+            print(ticker)
+            dict['portfolio'].append(product_dict)
 
     return dict
+
 
 def choose_crypto(ticker_df):
     base_currency = questionary.select(
@@ -43,10 +57,12 @@ def choose_crypto(ticker_df):
 
     return ticker
 
+
 def init_portfolio(dict):
     print(f"Let's get you set up.")
     dict['portfolio'] = []
     return build_portfolio(dict)
+
 
 def choose_market():
     market = questionary.select(
@@ -55,9 +71,26 @@ def choose_market():
         ).ask()
     return market
 
+
 def choose_crypto_exchange():
     exchange = questionary.select(
         "What exchange do you want to use?",
         choices = fh.crypto_exchange_list,
         ).ask()
     return exchange
+
+
+def add_funds(dict):
+    funds = questionary.text("How much do you want to invest?").ask()
+    try:
+        if 'balance' in dict:
+            dict['balance'] += float(funds)
+        else:
+            dict['balance'] = float(funds)
+    except:
+        print(f"{funds} is a invalid entry!")
+        if 'balance' not in dict:
+            dict['balance'] = 0
+    else:
+        print(funds)
+    return dict
--- user/user.py
+++ user/user.py
@@ -6,29 +6,48 @@ def main(display_string=None):
     if display_string is not None:
         print(display_string)
 
+    # Get username
     username = questionary.text("What is your name?").ask()
 
+    # Open shelf to access persistent data
     with shelve.open('../Resources/shelf') as sh:
+        # Check to see if username exists in shelf
         if username in sh:
             message = f"Hello, {username}!"
+        # If username does not exist, create empty dictionary
         else:
             sh[username] = {}
             message = f"It's nice to meet you, {username}!"
             sh.sync()
         print(message)
-
+        # Check to see if portfolio exists in shelf
         if 'portfolio' in sh[username]:
             print('Nice portfolio!', sh[username]['portfolio'])
             if not questionary.confirm("Do you want to keep it?").ask():
                 sh[username] = pb.init_portfolio(sh[username])
                 sh.sync()
-
+        # If portfolio does not exist, prompt user to create one
         else:
             print('You need to invest, homie!')
             sh[username] = pb.init_portfolio(sh[username])
             sh.sync()
+        
+        # Check to see if balance exists in shelf
+        if 'balance' in sh[username]:
+            if sh[username]['balance'] > 0.0:
+                print(f"Looking good ${sh[username]['balance']}!")
+            else:
+                print(f"Current balance ${sh[username]['balance']}.")
+            if questionary.confirm("Do you want to add funds?").ask():
+                sh[username] = pb.add_funds(sh[username])
+                sh.sync()
+        # If balaance does not exist, prompt user for input
+        else:
+            print(f"balance not in shelf for {username}")
+            sh[username] = pb.add_funds(sh[username])
+            sh.sync()
 
-        print(f"{sh[username]['portfolio']}")
+        print(f"{sh[username]['portfolio']}\nBalance: ${sh[username]['balance']}")
     return username
 
 if __name__ == "__main__":
