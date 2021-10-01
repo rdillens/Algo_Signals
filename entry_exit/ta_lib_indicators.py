83ac5c9bdd72bdc7e474df1874f98e3f2f1139a0
Scott Slusher <scottslusher22@gmail.com>
2021-10-01 15:26:45 -0500

    ta_lib indicators and atr logic

---
+++ entry_exit/ta_lib_indicators.py
@@ -0,0 +1,91 @@
+overlap_studies = {
+    "BBANDS":"Bollinger Bands",
+    "DEMA":"Double Exponential Moving Average",
+    "EMA":"Exponential Moving Average",
+    "HT_TRENDLINE":"Hilbert Transform - Instantaneous Trendline",
+    "KAMA":"Kaufman Adaptive Moving Average",
+    "MA":"Moving average",
+    "MAMA":"MESA Adaptive Moving Average",
+    "MAVP":"Moving average with variable period",
+    "MIDPOINT":"MidPoint over period",
+    "MIDPRICE":"Midpoint Price over period",
+    "SAR":"Parabolic SAR",
+    "SAREXT":"Parabolic SAR - Extended",
+    "SMA":"Simple Moving Average",
+    "T3":"Triple Exponential Moving Average (T3)",
+    "TEMA":"Triple Exponential Moving Average",
+    "TRIMA":"Triangular Moving Average",
+    "WMA":"Weighted Moving Average"
+}
+
+momentum_indicators = {
+    "ADX":"Average Directional Movement Index",
+    "ADXR":"Average Directional Movement Index Rating",
+    "APO":"Absolute Price Oscillator",
+    "AROON":"Aroon",
+    "AROONOSC":"Aroon Oscillator",
+    "BOP":"Balance Of Power",
+    "CCI":"Commodity Channel Index",
+    "CMO":"Chande Momentum Oscillator",
+    "DX":"Directional Movement Index",
+    "MACD":"Moving Average Convergence/Divergence",
+    "MACDEXT":"MACD with controllable MA type",
+    "MACDFIX":"Moving Average Convergence/Divergence Fix 12/26",
+    "MFI":"Money Flow Index",
+    "MINUS_DI":"Minus Directional Indicator",
+    "MINUS_DM":"Minus Directional Movement",
+    "MOM":"Momentum",
+    "PLUS_DI":"Plus Directional Indicator",
+    "PLUS_DM":"Plus Directional Movement",
+    "PPO":"Percentage Price Oscillator",
+    "ROC":"Rate of change : ((price/prevPrice)-1)*100",
+    "ROCP":"Rate of change Percentage: (price-prevPrice)/prevPrice",
+    "ROCR":"Rate of change ratio: (price/prevPrice)",
+    "ROCR100":"Rate of change ratio 100 scale: (price/prevPrice)*100",
+    "RSI":"Relative Strength Index",
+    "STOCH":"Stochastic",
+    "STOCHF":"Stochastic Fast",
+    "STOCHRSI":"Stochastic Relative Strength Index",
+    "TRIX":"1-day Rate-Of-Change (ROC) of a Triple Smooth EMA",
+    "ULTOSC":"Ultimate Oscillator",
+    "WILLR":"Williams' %R"
+}
+
+volumne_indicators = {
+    "AD":"Chaikin A/D Line",
+    "ADOSC":"Chaikin A/D Oscillator",
+    "OBV":"On Balance Volume",
+}
+
+volatility_indicators = {
+    "ATR":"Average True Range",
+    "NATR":"Normalized Average True Range",
+    "TRANGE":"True Range",
+}
+
+price_transform = {
+    "AVGPRICE":"Average Price",
+    "MEDPRICE":"Median Price",
+    "TYPPRICE":"Typical Price",
+    "WCLPRICE":"Weighted Close Price",
+}
+
+cycle_indicators = {
+    "HT_DCPERIOD":"Hilbert Transform - Dominant Cycle Period",
+    "HT_DCPHASE":"Hilbert Transform - Dominant Cycle Phase",
+    "HT_PHASOR":"Hilbert Transform - Phasor Components",
+    "HT_SINE":"Hilbert Transform - SineWave",
+    "HT_TRENDMODE":"Hilbert Transform - Trend vs Cycle Mode",
+}
+
+statistic_functions = {
+    "BETA":"Beta",
+    "CORREL":"Pearson's Correlation Coefficient (r)",
+    "LINEARREG":"Linear Regression",
+    "LINEARREG_ANGLE":"Linear Regression Angle",
+    "LINEARREG_INTERCEPT":"Linear Regression Intercept",
+    "LINEARREG_SLOPE":"Linear Regression Slope",
+    "STDDEV":"Standard Deviation",
+    "TSF":"Time Series Forecast",
+    "VAR":"Variance"
+}
